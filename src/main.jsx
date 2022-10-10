0eaa05f58e8b972ab6125285de944bab74ab7458
Dima Fiialo <dafw@live.ru>
2022-10-10 15:42:18 +0300

    complete code

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
-  RouterProvider
+  createRoutesFromElements,
+  RouterProvider,
+  Route
 } from "react-router-dom";
 import "./index.css";
 import Root, {
@@ -11,40 +13,87 @@ import Root, {
 } from './routes/root';
 import ErrorPage from "./error-page";
 import Contact, {
-    loader as contactLoader
+    loader as contactLoader,
+    action as contactAction
 } from "./routes/contact";
 import EditContact, {
     action as editAction
 } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root/>,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-        {
-            path: "contacts/:contactId",
-            element: <Contact />,
-            loader: contactLoader
-        },
-        {
-            path: "contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction
-        },
-        {
-            path: "contacts/:contactId/destroy",
-            action: destroyAction,
-        },
-    ]
-  }
-  
-]);
+// JSX variant of router
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        path="/"
+        element={<Root />}
+        loader={rootLoader}
+        action={rootAction}
+        errorElement={<ErrorPage />}
+      >
+        <Route errorElement={<ErrorPage />}>
+          <Route index element={<Index />} />
+          <Route
+            path="contacts/:contactId"
+            element={<Contact />}
+            loader={contactLoader}
+            action={contactAction}
+          />
+          <Route
+            path="contacts/:contactId/edit"
+            element={<EditContact />}
+            loader={contactLoader}
+            action={editAction}
+          />
+          <Route
+            path="contacts/:contactId/destroy"
+            action={destroyAction}
+          />
+        </Route>
+      </Route>
+    )
+  );
+
+// JS Object variant of router
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root/>,
+//     errorElement: <ErrorPage />,
+//     loader: rootLoader,
+//     action: rootAction,
+//     children: [
+//         {
+//             errorElement: <ErrorPage />,
+//             children: [
+//                 {
+//                     index: true,
+//                     element: <Index />
+//                 },
+//                 {
+//                     path: "contacts/:contactId",
+//                     element: <Contact />,
+//                     loader: contactLoader,
+//                     action: contactAction
+//                 },
+//                 {
+//                     path: "contacts/:contactId/edit",
+//                     element: <EditContact />,
+//                     loader: contactLoader,
+//                     action: editAction
+//                 },
+//                 {
+//                     path: "contacts/:contactId/destroy",
+//                     action: destroyAction,
+//                     errorElement: <div>Oops! There was an error.</div>,
+//                 },
+//             ]
+//         }
+//     ]
+//   }
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
